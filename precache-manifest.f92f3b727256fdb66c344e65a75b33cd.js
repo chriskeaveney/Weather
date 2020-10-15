778b56e967e368e7490c44f298f91691d1c200fa
Chris Keaveney <N00162019@student.iadt.ie>
2020-10-15 22:04:30 +0100

    Updates

---
+++ precache-manifest.f92f3b727256fdb66c344e65a75b33cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbafd8dca6c9df683fc22af6f077df73",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "0256115c1c904c953650",
+    "url": "/Weather/static/css/main.30ebac6f.chunk.css"
+  },
+  {
+    "revision": "573667e6ebe557781426",
+    "url": "/Weather/static/js/2.4a4cc568.chunk.js"
+  },
+  {
+    "revision": "cffa7461cc6f2dafc2a2594194ed1af9",
+    "url": "/Weather/static/js/2.4a4cc568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0256115c1c904c953650",
+    "url": "/Weather/static/js/main.dce47646.chunk.js"
+  },
+  {
+    "revision": "29f57d6435589858d753",
+    "url": "/Weather/static/js/runtime-main.c60773f4.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "7f5ac6166edf24e9704e8f0913078358",
+    "url": "/Weather/static/media/background.7f5ac616.jpg"
+  },
+  {
+    "revision": "5e31cae90bbad0694f7eee6d17876950",
+    "url": "/Weather/static/media/bc.5e31cae9.jpg"
+  },
+  {
+    "revision": "6fcc0476570acfea33ffc43744ba578a",
+    "url": "/Weather/static/media/city.6fcc0476.jpg"
+  },
+  {
+    "revision": "abcb3d57736b91a91e91f8b28278d43d",
+    "url": "/Weather/static/media/curate.abcb3d57.jpg"
+  },
+  {
+    "revision": "cdab85a392e92623afa27de32443379f",
+    "url": "/Weather/static/media/interact.cdab85a3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Weather/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a5f4236cc4e221cf84455160eaeb9c7c",
+    "url": "/Weather/static/media/logo.a5f4236c.png"
+  }
+]);
