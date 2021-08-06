ae185d569c225ef41ff3734ed2d5988fc9a287a9
Chris Keaveney <ck121@outlook.com>
2021-08-06 14:32:08 +0100

    changes3

--- src/components/currentLocation.js
+++ src/components/currentLocation.js
@@ -92,7 +92,7 @@ class Weather extends React.Component {
   // Async/Await used to fetch data from the API
   // After passing in coordinates, setState all the data gotten using API + display that data
   getWeather = async (lat, lon) => {
-    const api_call = await fetch(`${apiKeys.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKeys.key}`);
+    const api_call = await fetch(`${apiKeys.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=da473067a8a4d4276096998a9c272473`);
     const data = await api_call.json();
     this.setState({
       lat: lat,
